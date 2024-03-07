7970470bf6d3924c112d6cb682390e935f4d33b6
Wesley Stander <wesleyearlstander@gmail.com>
2024-03-07 19:24:13 +0000

    fix: some updates to tut 1-4.py

--- tut-4.py
+++ tut-4.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
@@ -9,13 +10,15 @@ def home():
 def login():
     if request.method == 'POST':
         user = request.form['nm']
-        return redirect(url_for('user', usr=user))
+        return redirect(url_for('user', user=user))
     else:
         return render_template("login.html")
 
-@app.route('/usr')
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route('/user')
+def user():
+    args_dict: dict = request.args
+    user: Optional[str] = args_dict.get('user', 'Default User')
+    return f"<h1>{user}</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
