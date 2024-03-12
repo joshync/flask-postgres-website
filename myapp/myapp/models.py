f9584af6782f6a056fbf1686f109e39ee69ac793
Wesley Stander <wesleyearlstander@gmail.com>
2024-03-12 17:31:02 +0000

    feature: some tidying up.

--- myapp/myapp/models.py
+++
@@ -1,8 +0,0 @@
-from myapp import db
-
-class Data(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return '<Data %r>' % self.content
--- myapp/run.py
+++
@@ -1,6 +0,0 @@
-from myapp import app, db
-from myapp.models import Data
-
-if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True) 
